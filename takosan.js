f53dc5e013c5ad8ac89d19b5aa2d1d35f2d00d8b
Makoto Chiba <hypermkt@gmail.com>
2018-01-31 01:23:42 +0900

    Babelでビルドできるように調整

---
+++ takosan.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var _axios = require('axios');
+
+var _axios2 = _interopRequireDefault(_axios);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }

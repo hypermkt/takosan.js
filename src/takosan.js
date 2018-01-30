22385ec8d9b0844478e8bb65c3673fa12c58df1f
Makoto Chiba <hypermkt@gmail.com>
2018-01-31 02:03:34 +0900

    動作確認はしていないがざっと書いた

--- src/takosan.js
+++ src/takosan.js
@@ -1,1 +1,31 @@
 import axios from 'axios';
+
+class Takosan {
+  set options(options) {
+    this.url = options.url;
+    this.channel = options.channel;
+    this.icon = options.icon;
+    this.name = options.name;
+  }
+
+  privmsg() {
+    let instance = axios.create({
+      baseUrl: this.url
+    });
+
+    axios.post('/privmsg', {
+      channel: this.channel,
+      message: this.message,
+      name: this.name,
+      icon: this.icon
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    }); 
+  }
+}
+
+export default Takosan;
--- takosan.js
+++ takosan.js
@@ -1,7 +1,39 @@
 'use strict';
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
 var _axios = require('axios');
 
 var _axios2 = _interopRequireDefault(_axios);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+class Takosan {
+  set options(options) {
+    this.url = options.url;
+    this.channel = options.channel;
+    this.icon = options.icon;
+    this.name = options.name;
+  }
+
+  privmsg() {
+    let instance = _axios2.default.create({
+      baseUrl: this.url
+    });
+
+    _axios2.default.post('/privmsg', {
+      channel: this.channel,
+      message: this.message,
+      name: this.name,
+      icon: this.icon
+    }).then(function (response) {
+      console.log(response);
+    }).catch(function (error) {
+      console.log(error);
+    });
+  }
+}
+
+exports.default = Takosan;
